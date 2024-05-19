55054a6f33b5b4f81c423e33a990c62406cffe34
Jakub Tutka <jakubtutka02@gmail.com>
2024-05-19 22:01:55 +0200

    logowanie google

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,42 @@
+<script setup>
+import { ref } from 'vue'
+import { auth } from '@/firebase'
+import { signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from 'firebase/auth'
+
+const user = ref(null)
+
+function login() {
+  const provider = new GoogleAuthProvider()
+
+  const onSuccess = (response) => {
+    console.log(response)
+  }
+
+  const onError = (error) => {
+    console.log(error)
+  }
+
+  signInWithPopup(auth, provider).then(onSuccess).catch(onError)
+}
+
+onAuthStateChanged(auth, currentUser => {
+  if (currentUser) {
+    console.log("logged in")
+    user.value = currentUser
+  }
+  else {
+    console.log("logged out")
+    user.value = null
+  }
+})
+</script>
+
 <template>
   Hello world
+
+  <br>
+
+  <button v-if="!user" @click="login">
+    Zaloguj się za pomocą Google
+  </button>
 </template>
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+
+const config = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  appId: import.meta.env.VITE_APP_ID,
+  authDomain: import.meta.env.VUE_AUTH_DOMAIN,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SEND_ID,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+}
+
+export const app = initializeApp(config)
+
+export const auth = getAuth(app)
