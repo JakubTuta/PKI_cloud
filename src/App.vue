6d6b581ae117062262188010d5f0e940ada3d232
Jakub Tutka <jakubtutka02@gmail.com>
2024-06-18 21:10:56 +0200

    zamiana js na ts

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import { ref } from 'vue'
 import { auth, firestore } from '@/firebase'
 import { signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut, FacebookAuthProvider, GithubAuthProvider } from 'firebase/auth'
